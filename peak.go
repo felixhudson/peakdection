0195f6c8e276a36cec8e98a743fc411b01c44b2b
felix@example.com <felix@example.com>
2017-08-15 16:41:55 +0930

    start of a package

---
+++ peak.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
+
+func Findpeaks(data []int) ([]int, error) {
+	// find all peaks in an array of data
+	result := []int{1}
+
+	return result, nil
+}
